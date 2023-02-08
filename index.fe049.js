7969e7ccf665cc0c5a159d5bc0758c40e204ba13
phucnds <nduongphuc@gmail.com>
2023-02-08 16:45:47 +0700

    - update

--- index.fe049.js
+++ index.fe049.js
@@ -1,4 +1,4 @@
-System.register(["./application.83b10.js"], function (_export, _context) {
+System.register(["./application.903b6.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.83b10.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application83b10Js) {
-      createApplication = _application83b10Js.createApplication;
+    setters: [function (_application903b6Js) {
+      createApplication = _application903b6Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
