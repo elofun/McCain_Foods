0d764e8ffdce3d2adc57bafdd68077349d17f3ea
phucnds <nduongphuc@gmail.com>
2023-02-09 14:20:51 +0700

    - update

--- index.32461.js
+++ index.32461.js
@@ -1,4 +1,4 @@
-System.register(["./application.1d551.js"], function (_export, _context) {
+System.register(["./application.34dc2.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.1d551.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application1d551Js) {
-      createApplication = _application1d551Js.createApplication;
+    setters: [function (_application34dc2Js) {
+      createApplication = _application34dc2Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
