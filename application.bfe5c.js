68d8d98dced21f3670b61af72402c4bd7d7b7433
phucnds <nduongphuc@gmail.com>
2023-02-16 13:51:06 +0700

    - update

--- application.bfe5c.js
+++ application.bfe5c.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
 
     function loadSettingsJson(cc) {
       var server = '';
-      var settings = 'src/settings.2b122.json';
+      var settings = 'src/settings.93b2d.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- index.eea04.js
+++ index.eea04.js
@@ -1,4 +1,4 @@
-System.register(["./application.958d7.js"], function (_export, _context) {
+System.register(["./application.bfe5c.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.958d7.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application958d7Js) {
-      createApplication = _application958d7Js.createApplication;
+    setters: [function (_applicationBfe5cJs) {
+      createApplication = _applicationBfe5cJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
